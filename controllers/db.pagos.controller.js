cf5fea98638986d2a733c94c131007424ca503ec
Mortis <cp06685@gmail.com>
2024-06-22 13:44:53 -0400

    rest api

--- controllers/db.pagos.controller.js
+++ controllers/db.pagos.controller.js
@@ -64,24 +64,10 @@ const getPagos= async(req, res) =>{
             
             };
 
-    const addPagos= async(req, res) =>{
-        try{
-            const { metodo, name } = req.body;
-            if (metodo == undefined || name == undefined ){
-                res.status(400).json({ message: "Bad Request. Please fill all field." });
-            }
-            const pagos={ metodo, name};
-            const connection = await getConnection();
-            const result = await connection.query("INSERT INTO pagos SET ?", pagos);
-            res.json(result);
-        } catch(error) {
-                res.status(500);
-                res.send(error.message);
-        }
-        
-        };
 
     export const methods ={
         getPagos,
-        addPagos
+        addPagos,
+        updatePagos,
+        deletePagos
         };
